c20fea746767bdce9b9b1aaaad3f0f1965d224d1
Taylor Hocutt <taylor.hocutt@gmail.com>
2015-03-04 23:37:07 -0500

    code should be written and implemented for the most part in my scene

---
+++ WhatLiesBelow/Assets/Scripts/Editor/ReorderLayers.cs
@@ -0,0 +1,70 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+// Creates an instance of a primitive depending on the option selected by the user.
+public class ReorderLayers : EditorWindow 
+{
+	public string[] options = new string[] {"foreground1", "foreground2", "foreground3"};
+	public int index = 0;
+
+	[MenuItem("CustomOptions/ReorderLayers")]
+	static void Init() 
+	{
+		EditorWindow window = GetWindow(typeof(ReorderLayers));
+		window.Show();
+	}
+
+
+	void OnGUI() 
+	{
+		index = EditorGUILayout.Popup(index, options);
+		if (GUILayout.Button("Execute"))
+			Reorder();
+		
+	}
+
+
+	void Reorder() 
+	{
+		SpriteRenderer[] renderers = FindObjectsOfType(typeof(SpriteRenderer)) as SpriteRenderer[];
+		int i = 0;
+		switch(index) 
+		{
+		case 0:  
+			foreach(SpriteRenderer sr in renderers)
+			{
+				if(sr.sortingLayerName == "foreground1")
+				{
+					sr.sortingOrder = i;
+					i++;
+				}
+					
+			}
+			break;
+		case 1:
+			foreach(SpriteRenderer sr in renderers)
+			{
+				if(sr.sortingLayerName == "foreground2")
+				{
+					sr.sortingOrder = i;
+					i++;
+				}
+			}
+			break;
+		case 2:
+			foreach(SpriteRenderer sr in renderers)
+			{
+				if(sr.sortingLayerName == "foreground3")
+				{
+					sr.sortingOrder = i;
+					i++;
+				}
+			}
+			break;
+		default:
+			Debug.LogError("Unrecognized Option");
+			break;
+		}
+	}
+}
