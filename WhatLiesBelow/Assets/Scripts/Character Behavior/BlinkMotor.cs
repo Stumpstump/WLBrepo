854bab12cd1e486c36dc6a47fac58910692f53eb
Taylor Hocutt <taylor.hocutt@gmail.com>
2015-04-16 18:50:19 -0400

    added a rough draft blink motor, and made jumping while moving down a slope more reliable

---
+++ WhatLiesBelow/Assets/Scripts/Character Behavior/BlinkMotor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlinkMotor : MonoBehaviour {
+
+	public float maxBlinkTime = 5f;
+	public float minBlinkTime = 0.3f;
+
+	private void Start()
+	{
+		StartCoroutine (Blink ());
+	}
+
+
+	private IEnumerator Blink()
+	{
+		float blinkTime = Random.Range (minBlinkTime, maxBlinkTime);
+		float elapsedTime = 0.0f;
+
+		while(elapsedTime < blinkTime)
+		{
+			elapsedTime += Time.deltaTime;
+			yield return new WaitForEndOfFrame();
+		}
+
+		Debug.Log ("blink : " + blinkTime);
+		StartCoroutine (Blink ());
+	}
+}
