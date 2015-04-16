9466f1bed8f0efeaa8d8b159e3951b693134d89e
Taylor Hocutt <taylor.hocutt@gmail.com>
2015-04-16 10:07:58 -0400

    fixed a couple bugs  - commented out random symbols added to character motor that caused compilation failures - removed debug log in jump to remove fps loss when jumping - fixed moss in main - copy scene, it was only missing a reference to the player and was sitting below the collider (remember hold x to gather moss)

--- WhatLiesBelow/Assets/Scripts/Character Movement/CharacterMotor.cs
+++ WhatLiesBelow/Assets/Scripts/Character Movement/CharacterMotor.cs
@@ -38,11 +38,11 @@ namespace WLB
 		private Vector2 move;
 		private Vector2 addedForceVector;
 
-<<<<<<< HEAD
-=======
+//<<<<<<< HEAD
+//=======
 
-<<<<<<< HEAD
->>>>>>> 6bf4137615947a9810a70055485dfea1b95148d2
+//<<<<<<< HEAD
+//>>>>>>> 6bf4137615947a9810a70055485dfea1b95148d2
 	//	private bool canDoubleJump = false;
 
 	//	public bool CanDoubleJump
@@ -54,8 +54,8 @@ namespace WLB
 	//		}
 	//	}
 
-=======
->>>>>>> master
+//=======
+//>>>>>>> master
 		private IEnumerator drainFatigue()
 		{
 			yield return new WaitForSeconds (0.1f);
@@ -116,17 +116,17 @@ namespace WLB
 				rigidBody2D.velocity = new Vector2(0f,0f);
 				rigidBody2D.AddForce (move);
 				//run jump function
-<<<<<<< HEAD
+//<<<<<<< HEAD
 				AudioHelper.CreatePlayAudioObject(SoundLibrary.water2);
-=======
+//=======
 				animationModule.SetState(AnimationModule.AnimationState.jumpingBlendTree);
-<<<<<<< HEAD
->>>>>>> 6bf4137615947a9810a70055485dfea1b95148d2
-				StartCoroutine(Jump());
-=======
+//<<<<<<< HEAD
+//>>>>>>> 6bf4137615947a9810a70055485dfea1b95148d2
+				//StartCoroutine(Jump());
+//=======
 				currentTime = 0;
 				isJumping = true;
->>>>>>> master
+//>>>>>>> master
 				//set canJump to false to disallow infinite jumping
 				canJump = false;
 			}
@@ -160,7 +160,6 @@ namespace WLB
 				}
 			}
 			vSpeed = rigidBody2D.velocity.y;
-			Debug.Log("vspeed is " + vSpeed);
 			//I need this for my blendtree to work. Refference unity video at 58:20
 		}
 
