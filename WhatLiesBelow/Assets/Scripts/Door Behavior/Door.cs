8e03598a066404225e351811d44d34ae126f0669
Taylor Hocutt <taylor.hocutt@gmail.com>
2015-05-07 18:19:19 -0400

    added base functionality for doors. doors can either move the player to another door within the scene or can load another scene by name.

---
+++ WhatLiesBelow/Assets/Scripts/Door Behavior/Door.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace WLB
+{
+	public class Door : MonoBehaviour 
+	{
+		public bool sceneTransition = false;
+		public Door exit;
+		public string scene;
+
+		public GameObject player;
+		public float coolDown = 3f;
+
+		public bool canTravel = true;
+
+		private void OnTriggerEnter2D(Collider2D other)
+		{
+			if(canTravel)
+			{
+				canTravel = false;
+				StartCoroutine(Cooldown());
+				if(sceneTransition)
+				{
+					Application.LoadLevel(scene);
+				}
+				else
+				{
+					exit.canTravel = false;
+					StartCoroutine(exit.Cooldown());
+					player.transform.position = exit.transform.position;
+				}
+			}
+		}
+
+		public IEnumerator Cooldown()
+		{
+			yield return new WaitForSeconds (coolDown);
+			canTravel = true;
+		}
+	}
+}
