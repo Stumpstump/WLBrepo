d6b4b90597ce9236001639474d84dbbe3f6b4c1d
Kevin Gregory <kevgrego@indiana.edu>
2015-05-18 19:14:39 -0400

    Added camera follow script (maybe?)

---
+++ WhatLiesBelow/Assets/Scripts/Camera Behavior/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public UnityEngine.Camera camera;
+	public GameObject player;
+	private float followx;
+	private float followy;
+	private float lastx;
+	private float lasty;
+	
+	// Update is called once per frame
+	void Update () {
+		followx = player.transform.position.x;
+		followy = player.transform.position.y;
+	}
+
+	void CameraGo(){
+		Debug.Log ("Camera Follow");
+		//camera.transform.position.x = followx;
+		//camera.transform.position.y = followy;
+		camera.transform.Translate (followx, followy, 0);
+	}
+
+	void CameraStop(){
+		Debug.Log ("Camera Stop");
+		lastx = player.transform.position.x;
+		lasty = player.transform.position.y;
+		//camera.transform.position.x = lastx;
+		//camera.transform.position.y = lasty;
+		camera.transform.Translate (lastx, lasty, 0);
+		}
+
+
+}
